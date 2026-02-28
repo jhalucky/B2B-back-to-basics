cf6fa16585e5f63287e168183bd87f95bf7b6fc3
Lucky Jha <jhalucky61@gmail.com>
2026-02-28 09:56:01 +0530

    file added

--- jsdailypractice/day1/truncateString.js
+++ jsdailypractice/day1/truncateString.js
@@ -1,5 +1,17 @@
-const truncateString = (str, length) => 
-    str.length > length ? str.slice(0, length) + "..." : str;
+// const truncateString = (str, length) => 
+//     str.length > length ? str.slice(0, length) + "..." : str;
 
 
-console.log(truncateString("ARYAN IS LODU", 5));
+// console.log(truncateString("ARYAN IS LODU", 5));
+
+
+// m2
+
+const truncateString = (str, length) => {
+    if(str.length <= length) {
+        return str;
+    }
+    return str.slice(0, length) + "..."
+}
+
+console.log(truncateString("bsdk, boht saaf dil ke", "4"));
