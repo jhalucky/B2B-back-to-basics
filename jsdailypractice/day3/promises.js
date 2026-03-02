af369a26a5b7b3bff7cfacc0f97a0f4c753ca346
Lucky Jha <jhalucky61@gmail.com>
2026-03-02 17:51:00 +0530

    file added

--- jsdailypractice/day3/promises.js
+++ jsdailypractice/day3/promises.js
@@ -2,11 +2,22 @@ const promiseNoOne = new Promise(function(resolve, reject) {
     setTimeout(function(){
          console.log("fuck you")
          resolve()
-    },5000)
+    },1000)
 })
 
 // promiseNoOne.then(console.log("Fuck you two times"));
 
 promiseNoOne.then(function() {
     console.log("fuck you twice a day");
+})
+
+new Promise(function(resolve, reject) {
+    setTimeout(function() {
+        console.log("Promise 2 needs to be resolved")
+        resolve()
+    },1000)
+}). then(function() {
+    setTimeout(function() {
+        console.log("Promise 2 resolved");
+    },1000)
 })
