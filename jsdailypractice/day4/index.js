8a207fef0a33b99cc342d29587a386d805e5658e
Lucky Jha <jhalucky61@gmail.com>
2026-03-09 19:34:11 +0530

    totalMarks.js

---
+++ jsdailypractice/day4/index.js
@@ -0,0 +1,11 @@
+// How to calculate total marks out of given arr of marks
+
+// const marks = [1,2,2,1,1,4,5,5,4]
+
+// let totalMarks = 0
+
+// for (let i = 0; i < marks.length; i++) {
+//     totalMarks+=marks[i]
+// }
+
+// console.log(totalMarks);
