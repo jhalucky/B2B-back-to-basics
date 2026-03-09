3eb407cecd203f73a4de66f4908fe394a906184f
Lucky Jha <jhalucky61@gmail.com>
2026-03-09 22:21:40 +0530

    countVowels.js

--- jsdailypractice/day4/countVowels.js
+++ jsdailypractice/day4/countVowels.js
@@ -1,15 +1,30 @@
+// using for of loop
+
 const word = "ANONymous"
 
 const vowels = ['a','e','i','o','u']
 
-function countVowels(word, vowels) {
-    let count = 0;
-    for (const w of word.toLowerCase()) {
-        if (vowels.includes(w)){
-            count++;
-        }
+// function countVowels(word, vowels) {
+//     let count = 0;
+//     for (const w of word.toLowerCase()) {
+//         if (vowels.includes(w)){
+//             count++;
+//         }
+//     }
+//     return count;
+// }
+
+// console.log(countVowels(word, vowels))
+
+
+// using for loop
+
+let count = 0;
+
+for (let i = 0; i < word.length; i++) {
+    if(vowels.includes(word[i])){
+        count++;
     }
-    return count;
 }
 
-console.log(countVowels(word, vowels))
+console.log(count);
