99691cb07f8f9eeedde29138e4b3e883e1f3d35e
Lucky Jha <jhalucky61@gmail.com>
2026-03-10 21:23:44 +0530

    getDayandDate.js

---
+++ jsdailypractice/day5/getDayandDate.js
@@ -0,0 +1,55 @@
+// Write a JavaScript program to display the current day and time in the following format.
+// Today is : Tuesday.
+// Current time is : 10 PM : 30 : 38
+
+
+var today = new Date();
+
+var day = today.getDay()
+
+// console.log(day);
+
+const dayList = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+
+console.log("Today is: " + dayList[day] + ".")
+
+
+var hours = today.getHours();
+var minutes = today.getMinutes();
+var seconds = today.getSeconds();
+
+// console.log(hours + ":" + minutes + ":" + seconds)
+
+var prepand = (hours >= 12) ? "PM" : "AM"
+
+hours = (hours >= 12) ? hours - 12 : hours; //to change into 12-hr format
+
+
+// special cases
+
+// when hr is 0
+
+if(hours === 0 && prepand === "PM") {
+    if (minutes === 0 && seconds === 0) {
+        hours = 12;
+        prepand = "Noon";
+    } else {
+        hours = 12;
+        prepand = 'PM'
+    }
+}
+
+// when hr is 0
+if(hours === 0 && prepand === "AM") {
+    if (minutes === 0 && seconds === 0) {
+        hours = 12;
+        prepand = "Midnight";
+    } else {
+        hours = 12;
+        prepand = 'AM'
+    }
+}
+
+console.log("Current Time: "+ hours + " " + prepand + " : " + minutes + " : " + seconds);
+
+// console.log(today)
