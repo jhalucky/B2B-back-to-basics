d9f7c840877853534bb0dd786a80d5a001f5b949
Lucky Jha <jhalucky61@gmail.com>
2026-03-17 22:44:44 +0530

    interview practice

--- jsdailypractice/day6-interviewpractice/practice.js
+++ jsdailypractice/day6-interviewpractice/practice.js
@@ -1,5 +1,6 @@
 // palindrome String
 
+// method 1: using if-else
 // function reverseString(str) {
 //     let reversedStr = str.split('').reverse().join('')
 //     if(reversedStr === str) {
@@ -10,11 +11,20 @@
 //     }
 // }
 
+// method 2: using ternary operator
+// const reverseString = (str) => {
+//   let reversedStr = str.split('').reverse().join('')
+//   console.log(reversedStr === str ? 'its palindrome' : 'not palindrome');
+// }
 
-const reverseString = (str) => {
-  let reversedStr = str.split('').reverse().join('')
-  
-}
+// let str = "fuckoff"
+// reverseString(str)
 
-let str = "fuckoff"
-reverseString(str)
+
+
+// method 3: ternary inline operator
+const isPalindrome = (str) => str === str.split('').reverse().join('')
+
+console.log(isPalindrome("fuckoff") ? 'yes' : 'no');
+
+
