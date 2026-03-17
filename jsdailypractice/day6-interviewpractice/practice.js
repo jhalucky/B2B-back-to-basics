156c395ff96dc91e82dc9daf42f013aca84eddc6
Lucky Jha <jhalucky61@gmail.com>
2026-03-17 22:36:01 +0530

    interview practice

---
+++ jsdailypractice/day6-interviewpractice/practice.js
@@ -0,0 +1,20 @@
+// palindrome String
+
+// function reverseString(str) {
+//     let reversedStr = str.split('').reverse().join('')
+//     if(reversedStr === str) {
+//         console.log("true");
+//     } else {
+//         console.log("false");
+        
+//     }
+// }
+
+
+const reverseString = (str) => {
+  let reversedStr = str.split('').reverse().join('')
+  
+}
+
+let str = "fuckoff"
+reverseString(str)
