f083dd7e72546b9c7f170d53725ed772918e89e6
Lucky Jha <jhalucky61@gmail.com>
2026-03-26 19:24:29 +0530

    Practice session

---
+++ jsrevision/firstques.js
@@ -0,0 +1,12 @@
+const product = {
+    productCategory: "Pen",
+    productName: "Parker Jotter Standard",
+    productPrice: 285,
+    isOfferAvailable: true,
+    discountPercent: 5, 
+    offerPrice: 270,
+    starsOnProduct: 4,
+    totalreviews: 7002
+}
+
+console.log(product);
